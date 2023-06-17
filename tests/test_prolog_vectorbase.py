41622c23519512260a8ce79a193d60b1d41467f5
Emily Ingram <lmingram@ualberta.ca>
2023-06-17 13:53:55 -0600

    added new property

--- tests/test_prolog_vectorbase.py
+++ tests/test_prolog_vectorbase.py
@@ -1,5 +1,6 @@
+import pytest
 from hypothesis import given, assume, strategies as st
-from vector_prolog.vectorbase import PrologVectorbase
+from vector_prolog.vectorbase import PrologVectorbase, RelationError
 
 def test_init_should_take_no_arguments():
     PrologVectorbase()
@@ -12,5 +13,12 @@ def test_add_relationship_should_be_callable():
 
 @given(name=st.text(), atoms=st.lists(elements=st.one_of(st.integers(), st.floats(), st.text())))
 def test_add_relationship_takes_a_string_and_multiple_atoms(name, atoms):
+    assume(len(atoms) > 0)
     vectorbase = PrologVectorbase()
     vectorbase.add_relationship(name, *atoms)
+
+@given(name=st.text())
+def test_add_relationship_raises_relation_error_when_0_atoms_given(name):
+    vectorbase = PrologVectorbase()
+    with pytest.raises(RelationError):
+        vectorbase.add_relationship(name)
--- vector_prolog/vectorbase.py
+++ vector_prolog/vectorbase.py
@@ -1,6 +1,10 @@
 from typing import Any
 
+class RelationError(Exception):
+    ...
+
 
 class PrologVectorbase:
     def add_relationship(self, name: str, *atoms: Any):
-        ...
+        if len(atoms) == 0:
+            raise RelationError("relationship must have at least one atom")
